5df7e09636d23e21c6a2bfe2ffbb1a112a6480cd
josh <jkneubuh@us.ibm.com>
2022-12-03 11:20:45 -0500

    gofmt
    Signed-off-by: josh <jkneubuh@us.ibm.com>

--- 2022/day3/3a.go
+++ 2022/day3/3a.go
@@ -4,65 +4,65 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"strings"
 	"os"
-    "unicode"
+	"strings"
+	"unicode"
 )
 
 var input = flag.String("in", "day3/input", "Input file")
 
 type Rucksack struct {
-    Top, Bottom string
+	Top, Bottom string
 }
 
 func main() {
-    fmt.Println("--- Day 3: Rucksack Ruckus ---")
+	fmt.Println("--- Day 3: Rucksack Ruckus ---")
 
-    flag.Parse()
+	flag.Parse()
 
-    sacks, err := load(*input)
-    if err != nil {
-        panic(err)
-    }
+	sacks, err := load(*input)
+	if err != nil {
+		panic(err)
+	}
 
-    total := 0
-    for _, sack := range sacks {
-        fmt.Printf("%-24s\t%-24s", sack.Top, sack.Bottom)
+	total := 0
+	for _, sack := range sacks {
+		fmt.Printf("%-24s\t%-24s", sack.Top, sack.Bottom)
 
-        common   := intersect(sack.Top, sack.Bottom)
-        priority := priority(common)
+		common := intersect(sack.Top, sack.Bottom)
+		priority := priority(common)
 
-        fmt.Printf("\t%d (%c)\n", priority, common)
+		fmt.Printf("\t%d (%c)\n", priority, common)
 
-        total += priority
-    }
+		total += priority
+	}
 
-    fmt.Printf("======================================\n")
-    fmt.Printf("Reorganized %d rucksacks.\n", len(sacks))
-    fmt.Printf("Mismatch sum: %d\n", total)
+	fmt.Printf("======================================\n")
+	fmt.Printf("Reorganized %d rucksacks.\n", len(sacks))
+	fmt.Printf("Mismatch sum: %d\n", total)
 }
 
 func priority(item rune) int {
-    if unicode.IsUpper(item) {
-        return int(item - 'A') + 1 + 26
+	if unicode.IsUpper(item) {
+		return int(item-'A') + 1 + 26
 
-    } else {
-        return int(item - 'a') + 1
-    }
+	} else {
+		return int(item-'a') + 1
+	}
 }
 
 func intersect(c1, c2 string) rune {
-    for _, r := range c1 {
-        if strings.ContainsRune(c2, r) {
-            return r
-        }
-    }
+	for _, r := range c1 {
+		if strings.ContainsRune(c2, r) {
+			return r
+		}
+	}
 
-    return '?'
+	return '?'
 }
 
 func load(file string) ([]*Rucksack, error) {
-    sacks := []*Rucksack{}
+	sacks := []*Rucksack{}
 
 	f, err := os.Open(file)
 	if err != nil {
@@ -70,22 +70,21 @@ func load(file string) ([]*Rucksack, error) {
 	}
 	defer f.Close()
 
-    scanner := bufio.NewScanner(f)
-    for scanner.Scan() {
-        line := scanner.Text()
-        runes := []rune(line)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		runes := []rune(line)
 
-        top    := string(runes[:len(runes)/2])
-        bottom := string(runes[len(runes)/2:])
+		top := string(runes[:len(runes)/2])
+		bottom := string(runes[len(runes)/2:])
 
-        sack := &Rucksack{
-            Top: top,
-            Bottom: bottom,
-        }
+		sack := &Rucksack{
+			Top:    top,
+			Bottom: bottom,
+		}
 
-        sacks = append(sacks, sack)
-    }
+		sacks = append(sacks, sack)
+	}
 
-    return sacks, nil
+	return sacks, nil
 }
-
--- 2022/day3/3b.go
+++ 2022/day3/3b.go
@@ -4,9 +4,9 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"strings"
 	"os"
-    "unicode"
+	"strings"
+	"unicode"
 )
 
 var input = flag.String("in", "day3/input", "Input file")
@@ -14,61 +14,50 @@ var input = flag.String("in", "day3/input", "Input file")
 type Squad [3]string
 
 func main() {
-    fmt.Println("--- Day 3: Rucksack Ruckus ---")
+	fmt.Println("--- Day 3: Rucksack Ruckus ---")
 
-    flag.Parse()
+	flag.Parse()
 
-    squads, err := load(*input)
-    if err != nil {
-        panic(err)
-    }
+	squads, err := load(*input)
+	if err != nil {
+		panic(err)
+	}
 
-    total := 0
-    for _, squad := range squads {
-        fmt.Printf("%-32s\t", squad)
+	total := 0
+	for _, squad := range squads {
+		fmt.Printf("%-32s\t", squad)
 
-        badge    := intersect(squad)
-        priority := priority(badge)
+		badge := intersect(squad)
+		priority := priority(badge)
 
-        fmt.Printf("\t%d (%c)\n", priority, badge)
+		fmt.Printf("\t%d (%c)\n", priority, badge)
 
-        total += priority
-    }
+		total += priority
+	}
 
-    fmt.Printf("======================================\n")
-    fmt.Printf("Reorganized %d squads.\n", len(squads))
-    fmt.Printf("Badge priority sum: %d\n", total)
+	fmt.Printf("======================================\n")
+	fmt.Printf("Reorganized %d squads.\n", len(squads))
+	fmt.Printf("Badge priority sum: %d\n", total)
 }
 
 func priority(item rune) int {
-    if unicode.IsUpper(item) {
-        return int(item - 'A') + 1 + 26
+	if unicode.IsUpper(item) {
+		return int(item-'A') + 1 + 26
 
-    } else {
-        return int(item - 'a') + 1
-    }
+	} else {
+		return int(item-'a') + 1
+	}
 }
 
 func intersect(squad Squad) rune {
-    // map intersection would be better
-    for _, r := range squad[0] {
-        if strings.ContainsRune(squad[1], r) && strings.ContainsRune(squad[2], r) {
-            return r
-        }
-    }
+	// map intersection would be better
+	for _, r := range squad[0] {
+		if strings.ContainsRune(squad[1], r) && strings.ContainsRune(squad[2], r) {
+			return r
+		}
+	}
 
-    return '?'
-}
-
-
-func intersec_t(c1, c2 string) rune {
-    for _, r := range c1 {
-        if strings.ContainsRune(c2, r) {
-            return r
-        }
-    }
-
-    return '?'
+	return '?'
 }
 
 func load(file string) ([]Squad, error) {
@@ -79,22 +68,21 @@ func load(file string) ([]Squad, error) {
 	}
 	defer f.Close()
 
-    squads := []Squad{}
+	squads := []Squad{}
 
-    scanner := bufio.NewScanner(f)
-    for scanner.Scan() {
-        sack1 := scanner.Text()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		sack1 := scanner.Text()
 
-        scanner.Scan()
-        sack2 := scanner.Text()
+		scanner.Scan()
+		sack2 := scanner.Text()
 
-        scanner.Scan()
-        sack3 := scanner.Text()
+		scanner.Scan()
+		sack3 := scanner.Text()
 
-        squad := [3]string{ sack1, sack2, sack3 }
-        squads = append(squads, squad)
-    }
+		squad := [3]string{sack1, sack2, sack3}
+		squads = append(squads, squad)
+	}
 
-    return squads, nil
+	return squads, nil
 }
-
